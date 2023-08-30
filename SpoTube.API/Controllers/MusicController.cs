1a3866b48c02cde5ca30c616311e6a5bf8d39e89
estaciohugodev <estaciovitorhugo@gmail.com>
2023-08-29 21:58:44 -0300

    Initial commit

---
+++ SpoTube.API/Controllers/MusicController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpoTube.API.Controllers
+{
+    [ApiController]
+    [Route("api/music")]
+    public class MusicController : ControllerBase
+    {
+        
+    }
+}
